79a97baa989283d31798987f1872deb5b2d2ed64
yamaguchi_shota <yamaguchi_shota@kakaku.com>
2019-10-31 11:21:03 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 山口 彰太 on 2019/10/31.
+//  Copyright © 2019 shouta.yamaguchi4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
